2a059b635deff83e022141951d3d0464c19f31bb
知念英弥 <s19013@std.it-college.ac.jp>
2023-06-11 20:32:01 +0900

    ビルド

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
-  // outputDir: 'docs',
+  outputDir: 'docs',
+  publicPath: '/simple-math-vue/',
   // publicPath: './',
-  // publicPath: '/simple-math-vue/',
 })
