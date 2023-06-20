15cbae485fcf2cc36989584f70f22c571cc1b93b
知念英弥 <s19013@std.it-college.ac.jp>
2023-06-20 16:35:43 +0900

    composition api化

--- src/components/ValueInput.vue
+++ src/components/ValueInput.vue
@@ -4,19 +4,19 @@
   </div>
 </template>
 
-<script>
-export default {
-  data() {
-    return {
-        value:0
-    }
-  },
-  watch:{
-    value:function(newValue){
-        this.$emit('changeValue',{newValue:newValue})
-    }
-  },
-}
+<script setup>
+import { ref,watch } from "vue";
+import { defineEmits } from 'vue';
+
+const value = ref(0);
+
+// emitは前もって色々宣言しておく必要がある
+// 配列の中に関数名じゃんじゃyん入れておけばok
+const emit = defineEmits(['changeValue'])
+
+watch(value,(newValue,oldValue) => {
+  emit('changeValue',{newValue:newValue})
+})
 </script>
 
 <style scoped>
